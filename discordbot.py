33d5aa79fe17f065d0d174317dacadf4848b1e4c
Logger-web <77101780+Logger-web@users.noreply.github.com>
2021-01-07 19:49:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,8 +14,8 @@ async def on_command_error(ctx, error):
 
 
 @bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+async def こんばんは(ctx):
+    await ctx.send('こんばんは(　＾∀＾)')
 
 
 bot.run(token)
