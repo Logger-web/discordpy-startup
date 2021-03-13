e203f407f40107906be5cccbed90b176eee2786c
Logger-web <77101780+Logger-web@users.noreply.github.com>
2021-03-13 18:18:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,40 +5,32 @@ import traceback
 bot = commands.Bot(command_prefix='hi!')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@client.command()
-async def tempmute(ctx, member: discord.Member, time: int, d, *, reason=None):
+@bot.command(description="Mutes the specified user.")
+@commands.has_permissions(manage_messages=True)
+async def mute(ctx, member: discord.Member, *, reason=None):
     guild = ctx.guild
+    mutedRole = discord.utils.get(guild.roles, name="Muted")
 
-    for role in guild.roles:
-        if role.name == "Muted":
-            await member.add_roles(role)
+    if not mutedRole:
+        mutedRole = await guild.create_role(name="Muted")
 
-            embed = discord.Embed(title="muted!", description=f"{member.mention} has been tempmuted ", colour=discord.Colour.light_gray())
-            embed.add_field(name="reason:", value=reason, inline=False)
-            embed.add_field(name="time left for the mute:", value=f"{time}{d}", inline=False)
-            await ctx.send(embed=embed)
+        for channel in guild.channels:
+            await channel.set_permissions(mutedRole, speak=False, send_messages=False, read_message_history=True, read_messages=False)
 
-            if d == "s":
-                await asyncio.sleep(time)
+    await member.add_roles(mutedRole, reason=reason)
+    await ctx.send(f"Muted {member.mention} for reason {reason}")
+    await member.send(f"You were muted in the server {guild.name} for {reason}")
 
-            if d == "m":
-                await asyncio.sleep(time*60)
+@bot.command(description="Unmutes a specified user.")
+@commands.has_permissions(manage_messages=True)
+async def unmute(ctx, member: discord.Member):
+    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
 
-            if d == "h":
-                await asyncio.sleep(time*60*60)
-
-            if d == "d":
-                await asyncio.sleep(time*60*60*24)
-
-            await member.remove_roles(role)
-
-            embed = discord.Embed(title="unmute (temp) ", description=f"unmuted -{member.mention} ", colour=discord.Colour.light_gray())
-            await ctx.send(embed=embed)
-
-            return
-
-
-
+    await member.remove_roles(mutedRole)
+    await ctx.send(f"Unmuted {member.mention}")
+    await member.send(f"You were unmuted in the server {ctx.guild.name}")
+    
+    
 @bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
